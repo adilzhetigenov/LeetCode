d8e3c58c65b448afe3a3d02e9d840bf911a2a14a
[adishata] <jetigenovadil@gmail.com>
2025-03-21 14:04:10 +0100

    implemented hashmap

--- 20. Valid Parentheses/sol.py
+++ 20. Valid Parentheses/sol.py
@@ -1,28 +1,42 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        hashmap = {')':'(','}':'{',']':'['}
         stack = []
 
-        for i in s:
-            match i:
-                case '(':
-                    stack.append('(')
-                case '{':
-                    stack.append('{')
-                case '[':
-                    stack.append('[')
-                case ')':
-                    if(len(stack)>0 and stack[-1] == '('):
-                        stack.pop()
-                    else:
+        for c in s:
+            if c not in hashmap:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+
+                else:
+                    k = stack.pop()
+                    if k!=hashmap[c]:
                         return False
-                case '}':
-                    if(len(stack)>0 and  stack[-1] == '{'):
-                        stack.pop()
-                    else:
-                        return False
-                case ']':
-                    if(len(stack)>0 and stack[-1] == '['):
-                        stack.pop()
-                    else:
-                        return False
-        return len(stack) == 0
+        return not stack            
+
+        # for i in s:
+        #     match i:
+        #         case '(':
+        #             stack.append('(')
+        #         case '{':
+        #             stack.append('{')
+        #         case '[':
+        #             stack.append('[')
+        #         case ')':
+        #             if(len(stack)>0 and stack[-1] == '('):
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         case '}':
+        #             if(len(stack)>0 and  stack[-1] == '{'):
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         case ']':
+        #             if(len(stack)>0 and stack[-1] == '['):
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        # return len(stack) == 0
