c6c27fe63356ceec014e2dcec4a4bee5fafb2346
[adishata] <jetigenovadil@gmail.com>
2026-01-03 16:29:34 +0100

    solved with dp inplace

--- 53.maximum-subarray/53.maximum-subarray.py
+++ 53.maximum-subarray/53.maximum-subarray.py
@@ -7,27 +7,61 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        post = nums[:]
-        pre = nums[:]
-
         n = len(nums)
 
-        for i in range(1, n):
-            pre[i] += pre[i-1]
-        for i in range(n-2,-1,-1):
-            post[i] += post[i+1]
+        maxSum = float("-inf")
+        curSum = 0
 
-        preMaxi = 0
-        postMaxi = n-1
-        for i in range(1,n):
-            if pre[preMaxi] < pre[i]:
-                preMaxi = i
-            if post[postMaxi] < post[n-i-1]:
-                postMaxi = n-1-i 
+        for i,val in enumerate(nums):
+            if val < val + curSum:
+                curSum += val
+                nums[i] = curSum
+            else:
+                curSum = val
+                
+            if maxSum < curSum:
+                maxSum = curSum
 
-        print(f"pre:{pre}\npost:{post}\nsubArr:{preMaxi},{postMaxi}")
-        return sum(nums[postMaxi:preMaxi+1])
+        return maxSum
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if len(nums) == 1:
+        #     return nums[0]
+        # post = nums[:]
+        # pre = nums[:]
+
+        # n = len(nums)
+
+        # for i in range(1, n):
+        #     pre[i] += pre[i-1]
+        # for i in range(n-2,-1,-1):
+        #     post[i] += post[i+1]
+
+        # preMaxi = 0
+        # postMaxi = n-1
+        # for i in range(1,n):
+        #     if pre[preMaxi] < pre[i]:
+        #         preMaxi = i
+        #     if post[postMaxi] < post[n-i-1]:
+        #         postMaxi = n-1-i 
+        # if preMaxi == len(nums)-1:
+        #     preMaxi -=1
+        # print(f"pre:{pre}\npost:{post}\nsubArr:{preMaxi},{postMaxi}")
+        # res = sum(nums[postMaxi:preMaxi+1])
+
+        # return res
+       
         
 
         # if len(nums) == 1:
