043f387de55281976a25d74e82a047d2255c57d7
[adishata] <jetigenovadil@gmail.com>
2026-01-06 23:59:05 +0100

    optimized solution

--- 338.counting-bits/338.counting-bits.py
+++ 338.counting-bits/338.counting-bits.py
@@ -7,27 +7,26 @@
 # @lc code=start
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res = [0]
         # border = 2
         # curNumOfOnes = 0
         # for i in range(1,n+1):
         #     curNumOfOnes+=1
         #     if curNumOfOnes == border:
         #         curNumOfOnes = 1
-        #         border +=1 
+        #         border +=1    
         #     print(f"{i}:{border}")
         #     res.append(curNumOfOnes)
+        res = [0]*(n+1)
 
-        powCnt = 0
-        nextChange = 2
+        nextChange = 1
         for i in range(1,n+1):
 
-            if i == nextChange:
-                powCnt+=1
-                nextChange*=2
+            if i == nextChange*2:
+                
+                nextChange=i
 
-            curNumOfOnes = 1 + res[i-2**powCnt]
-            res.append(curNumOfOnes)
+            curNumOfOnes = 1 + res[i-nextChange]
+            res[i]=curNumOfOnes
 
         return res
 # @lc code=end
